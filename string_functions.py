facc91fe2a41339853e7e89b8940192e0ff885f0
Arsen Diubua <arsenium1941@gmail.com>
2022-03-28 14:07:40 +0500

    28.03.2022_1

---
+++ string_functions.py
@@ -0,0 +1,83 @@
+from sqlalchemy import func
+
+from core.db import SessionLocal
+from core.models import Customers
+
+
+# SELECT address || ' / ' ||city
+# FROM customers
+# LIMIT 10
+
+def get_concat():
+    with SessionLocal() as session:
+        query_answer = session.query(func.concat(Customers.c.address, ' / ', Customers.c.city)
+                                     ).limit(10)
+
+        for item in query_answer:
+            print(item)
+        print('-----------------------SQL-Query--------------------------------')
+        return query_answer
+
+
+# SELECT  address, char_length(address)
+# FROM customers
+# LIMIT 10
+
+def get_char_length():
+    with SessionLocal() as session:
+        query_answer = session.query(Customers.c.address, func.char_length(Customers.c.address)
+                                     ).limit(10)
+
+        for item in query_answer:
+            print(item)
+        print('-----------------------SQL-Query--------------------------------')
+        return query_answer
+
+
+# SELECT  upper(address), lower(address)
+# FROM customers
+# LIMIT 10
+
+def get_upper_and_lower():
+    with SessionLocal() as session:
+        query_answer = session.query(func.upper(Customers.c.address), func.lower(Customers.c.address)
+                                     ).limit(10)
+
+        for item in query_answer:
+            print(item)
+        print('-----------------------SQL-Query--------------------------------')
+        return query_answer
+
+
+# SELECT  customer_id, overlay(customer_id placing 'AA' from 1 for 2)
+# FROM customers
+# LIMIT 10
+
+
+def get_overlay():
+    with SessionLocal() as session:
+        query_answer = session.query(Customers.c.customer_id, func.overlay(Customers.c.customer_id, 'AA', 1, 2)
+                                     ).limit(10)
+
+        for item in query_answer:
+            print(item)
+        print('-----------------------SQL-Query--------------------------------')
+        return query_answer
+
+
+# SELECT  customer_id, substring(customer_id placing from 2 for 3)
+# FROM customers
+# LIMIT 10
+def get_substring():
+    with SessionLocal() as session:
+        query_answer = session.query(Customers.c.customer_id, func.substring(Customers.c.customer_id,  2, 3)
+                                     ).limit(10)
+
+        for item in query_answer:
+            print(item)
+        print('-----------------------SQL-Query--------------------------------')
+        return query_answer
+
+
+if __name__ == '__main__':
+    print(get_substring())
